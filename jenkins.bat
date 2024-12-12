f17fc7dab214d1fdebc2aedd172c11ff78cf293c
AlihanFemi <alihanfemiofficial@gmail.com>
2024-12-12 18:40:30 +0200

    Setting up the enviroment

---
+++ jenkins.bat
@@ -0,0 +1,8 @@
+C:\Python312\python.exe -m pip install -r C:\Users\Alihan\Desktop\Code\QA\ve\requirements.txt
+git init
+git config --global user.email "alihanfemiofficial@gmail.com"
+git config --global user.name "AlihanFemi"
+pre-commit install
+git add ./.pre-commit-config.yaml
+git add C:\Users\Alihan\Desktop\Code\QA\ve\swapi.py
+git commit -m "testing pre-commits"
