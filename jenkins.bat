f131a6e58af9bf8b022d29e974e8d42921340ef0
AlihanFemi <alihanfemiofficial@gmail.com>
2025-01-09 17:26:53 +0200

    Edited requreiments

--- jenkins.bat
+++ jenkins.bat
@@ -2,7 +2,11 @@ C:\Python312\python.exe -m pip install -r C:\Users\Alihan\Desktop\Code\QA\ve\req
 git init
 git config --global user.email "alihanfemiofficial@gmail.com"
 git config --global user.name "AlihanFemi"
+git remote add origin https://github.com/AlihanFemi/SOFTMETTEST.git
+git branch -M main
 pre-commit install
 git add ./.pre-commit-config.yaml
-git add C:\Users\Alihan\Desktop\Code\QA\ve\swapi.py
+git add C:\Users\Alihan\Desktop\Code\QA\SOFTMETTEST\main.py
+git add C:\Users\Alihan\Desktop\Code\QA\SOFTMETTEST\test_main.py
 git commit -m "testing pre-commits"
+git push -u origin main
