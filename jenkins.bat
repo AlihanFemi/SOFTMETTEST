a0a6dc1fb9365eba7728d101b8d1ec65ce5b45d6
AlihanFemi <alihanfemiofficial@gmail.com>
2025-01-09 17:58:11 +0200

    testing pre-commits

--- jenkins.bat
+++ jenkins.bat
@@ -1,22 +1,11 @@
+cd C:\Users\Alihan\Desktop\Code\QA\SOFTMETTEST
 C:\Python312\python.exe -m pip install -r C:\Users\Alihan\Desktop\Code\QA\ve\requirements.txt
-
-REM Ensure we're on a branch instead of detached HEAD
-git checkout main || git checkout -b main
-
-REM Configure Git user details
+git init
 git config --global user.email "alihanfemiofficial@gmail.com"
 git config --global user.name "AlihanFemi"
-
-REM Unset core.hooksPath to fix pre-commit issue
-git config --unset-all core.hooksPath
-
-REM Install pre-commit hooks
+git config --global --add safe.directory C:/Users/Alihan/Desktop/Code/QA/SOFTMETTEST
 pre-commit install
-
-REM Stage and commit changes
 git add ./.pre-commit-config.yaml
 git add -A
-git commit -m "testing pre-commits" || echo "No changes to commit."
-
-REM Push changes to the branch
-git push origin main
+git commit -m "testing pre-commits"
+git push
